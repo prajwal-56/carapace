bad465d5ecb0718f91c2863e23248a5cc79711fd
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-01-29 00:27:10 +0530

    Yay ! It does something

--- src/main.c
+++ src/main.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sched.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[] ){
 
-    int i = 0;
-    while (i < argc){
-        printf("\narg %d  : %s" , i , argv[i]);
-        i++;
+    if(argc < 2){
+        printf("huh !? pass some argunents (likely a bin file or smth like that )");
+    } else {
+        pid_t pid = fork();     
+
+        if (pid == -1){
+            perror("something went wrong. Forking Failed :(");
+            return 1;
+        }
+
+        if (pid == 0){      // A fork has been succesfully created
+            execvp(argv[1] , &argv[1]);
+
+            perror("Something went wrong (most likely execvp)");
+            exit(1);
+        } else {
+            wait(NULL);
+            printf("\nVoila !!!\n");
+        }
     }
 
     return 0;
