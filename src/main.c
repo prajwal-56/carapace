f01a5b427ea4e9c2d58246a259bd69251e96862b
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-03-26 14:52:30 +0530

    just realized that I was in the wrong branch

--- src/main.c
+++ src/main.c
@@ -5,14 +5,45 @@
 #include <sched.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define STACK_SIZE (1024 * 1024)
 
-int childStuff(void *arg)
-{
+// Creates a folder inside cgroups 
+void cgroups_init(pid_t pid, long memory_limit_in_bytes){
+
+    // create a new cgroup directory for the container
+    mkdir("/sys/fs/cgroup/carapace", 0755);
+
+    const char *memory_max_path = "/sys/fs/cgroup/carapace/memory.max";
+    const char *cgroup_procs_path = "/sys/fs/cgroup/carapace/cgroup.procs";
+
+    // writing into memory.max
+    FILE *memMaxFp = fopen(memory_max_path , "w");
+    if (memMaxFp == NULL){
+        perror("Couldn't allocate memory or open **memory.max** file. Try running with sudo :(");
+        return;
+    } 
+    fprintf(memMaxFp, "%ld", memory_limit_in_bytes);    
+    fclose(memMaxFp);
+    printf("---- memory limit set to %ld bytes -----\n", memory_limit_in_bytes);
+
+    // writing into cgroup.procs
+    FILE *cgroupProcsFp = fopen(cgroup_procs_path, "w");
+    if( cgroup_procs_path == NULL){
+        perror("Couldn't open **cgroup.procs** file. Try running with sudo :(");
+        return;
+    }
+    fprintf(cgroupProcsFp , "%d", pid);
+    fclose(cgroupProcsFp);
+    printf("---- added process with PID %d to cgroup -----\n", pid);
+}
+
+int childStuff(void *arg){
     char **argv = (char **)arg; // casts the array back into string array
 
-    printf("\nthe PID of Child : %d\n", getpid());
+    printf("\nthe PID of Child : %d\nParent ID : %d", getpid(), getppid());
 
     // To execute that command (user's command)
     execvp(argv[1], &argv[1]);
@@ -21,25 +52,23 @@ int childStuff(void *arg)
     return 1;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 
     char *stack = malloc(STACK_SIZE);
 
-    if (argc < 2)
-    {
+    if (argc < 2){
         printf("huh !? pass some argunents (likely a bin file or smth like that )");
-    }
-    else
-    {
-        pid_t pid = clone(childStuff, stack + STACK_SIZE, SIGCHLD, argv);
+    } else {
+        pid_t pid = clone(childStuff, stack + STACK_SIZE, CLONE_NEWPID | CLONE_NEWUTS | CLONE_NEWNS | SIGCHLD, argv);
 
-        if (pid == -1)
-        {
+        if (pid == -1){
             perror("something went wrong. Forking Failed :(");
             return 1;
         }
 
+        printf("---- cgroup thing working -----\n" );
+        printf("---- creating cgroup.memory.max - allocating 56MB -----\n" );
+        cgroups_init(pid, 56000000); // 56 MB)
         wait(NULL);
         printf("\nVoila !!!\n");
         free(stack);
